d7a1c934bd4cd04ee116a696a527d82666097a3e
Jean-Baptiste de Lanfranchi <jbdelanfranchi@gmail.com>
2022-06-10 11:17:36 +0200

    build options

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: "src/theme-generator.ts",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: /^lit/,
-    },
-  },
+  base: "/concordecolors/",
+  // build: {
+  //   lib: {
+  //     entry: "src/theme-generator.ts",
+  //     formats: ["es"],
+  //   },
+  //   // rollupOptions: {
+  //   //   external: /^lit/,
+  //   // },
+  // },
 });
