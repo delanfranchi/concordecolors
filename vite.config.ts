bb6f9b65ecee56acb6999648d33cc37bb52df05e
jb <jb@superoniks.com>
2023-11-20 08:43:20 +0100

    test build action

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
-import { defineConfig } from "vite";
-var postcssLit = require("rollup-plugin-postcss-lit");
+import {defineConfig} from 'vite';
+var postcssLit = require('rollup-plugin-postcss-lit');
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/concordecolors/",
-  resolve:{
-    alias:{
-      '@concorde-app/tailwind' : path.resolve(__dirname, './src/js/tailwind/sonic-tailwind'),
+  base: '/concordecolors/',
+  resolve: {
+    alias: {
+      '@concorde-app/tailwind': path.resolve(
+        __dirname,
+        './src/js/tailwind/sonic-tailwind'
+      ),
     },
   },
   server: {
     fs: {
       // Allow serving files from one level up to the project root
-      allow: [".."],
+      allow: ['..'],
     },
   },
   plugins: [
     postcssLit({
       include: [
-        "src/js/**/*.scss",
-        "src/js/**/*.scss?*",
-        "src/js/**/*.css",
-        "src/js/**/*.css?*",
+        'src/js/**/*.scss',
+        'src/js/**/*.scss?*',
+        'src/js/**/*.css',
+        'src/js/**/*.css?*',
       ],
     }),
   ],
+  //
   // build: {
   //   lib: {
   //     entry: "src/theme-generator.ts",
